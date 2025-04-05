0c99a603d1d8d50c64c40a6b082d3515c98922ee
Vlad_Deineko <vladd1904@gmail.com>
2025-04-05 18:41:58 +0300

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'public/index.html'),
-        gallery: path.resolve(__dirname, 'public/1-gallery.html'),
-        form: path.resolve(__dirname, 'public/2-form.html'),
+        main: path.resolve(__dirname, 'src/public/index.html'),
+        gallery: path.resolve(__dirname, 'src/public/1-gallery.html'),
+        form: path.resolve(__dirname, 'src/public/2-form.html'),
       }
     }
   }
-}); 
+});
